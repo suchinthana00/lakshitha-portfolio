927ed05550e36a738e80530f9aa3269b0cd09e1e
Suchinthana <32512017+Such19@users.noreply.github.com>
2023-08-06 02:00:33 +0530

    Initial commit

---
+++ src/content_option.js
@@ -0,0 +1,208 @@
+const logotext = "Lakshitha.live";
+const meta = {
+  title: "Lakshitha Weerakoon",
+  description:
+    "I’m Lakshitha Weerakoon profressional e-commerce developer from Sri Lanka",
+};
+
+const introdata = {
+  title: "I’m Lakshitha Weerakoon",
+  animated: {
+    first: "Shopify Web Development",
+    second: "Shopify App Development",
+    third: "Shopify Custom Developments",
+    fourth: "WordPress Web Development",
+    fifth: "UI/UX Design",
+  },
+  description:
+    "I'm a professional developer with years of experiance and serving hundreds of clients worldwide.",
+  your_img_url: "https://cknickbo.sirv.com/Images/profile3.jpg?thumbnail=300",
+};
+
+const dataabout = {
+  title: "abit about my self",
+  aboutme:
+    "I'm a professional developer with 4 years+ experiance in building e-commerce solutions with shopify and wordpress and served hundreds of customers around the globe in various fields. Since beginning my journey as a freelance designer nearby 4 years ago, I 've done remote work for agencies, consulted for startup, and collaborated with talented people to create digital products.",
+};
+// const worktimeline = [{
+//         jobtitle: "Browns deals",
+//         where: "Conglomerate online store",
+//         date: "2022",
+//     },
+//     {
+//         jobtitle: "Tusker teas",
+//         where: "Online tea store",
+//         date: "2021",
+//     },
+//     {
+//         jobtitle: "Troublemaker",
+//         where: "Online baby store",
+//         date: "2021",
+//     },
+// ];
+
+// const skills = [{
+//         name: "Shopify",
+//         value: 98,
+//     },
+//     {
+//         name: "Wordpress",
+//         value: 90,
+//     },
+//     {
+//         name: "UI/UX",
+//         value: 85,
+//     },
+//     {
+//         name: "Shopify Custom Coding",
+//         value: 85,
+//     },
+//     {
+//         name: "SEO",
+//         value: 79,
+//     },
+// ];
+
+const services = [
+  {
+    title: "Shopify Web Design",
+    description:
+      "Get your all e-commerce needs done with complete Shopify stores. Exprerianced over 120+ succecsful store creations. Store design from the scratch. Dropshipping & Directshipping store design. Shopify Consulting.",
+  },
+  {
+    title: "Shopify Custom Development",
+    description: "Shopify backend/liquid development. Custom app development.",
+  },
+  {
+    title: "Wordpress Design",
+    description:
+      "Wordpress solutions for all your needs. Including Custom WordPress development and Woocommorce development. I provide attracitve design solutions with dedicated to your requirements.",
+  },
+  {
+    title: "UI/UX Design",
+    description:
+      "Design products that provide users with meaningful experiences while visualizing your amazing web/ app ideas. I'll create it with stunning designs and remarkable value addition.",
+  },
+];
+
+const dataportfolio = [
+  {
+    name: "Tusker Tea",
+    img: "https://cknickbo.sirv.com/Images/tinified/Tusker%20Tea.png",
+    desctiption:
+      "The Australian tea retailer store that provide with authentic design that expand their sales",
+    link: "https://tuskertea.com.au/",
+  },
+  {
+    name: "Browns Deals",
+    img: "https://cknickbo.sirv.com/Images/tinified/browns.png",
+    desctiption: "Enterprise with thousands of items",
+    link: "https://brownsdeals.com/",
+  },
+  {
+    name: "Spa Ceylon New Zealand",
+    img: "https://cdn.shopify.com/s/files/1/0796/3469/4430/files/Screenshot_2023-08-04_at_7.52.56_PM.png?v=1691159006",
+    desctiption:
+      "The Spa Ceylon range of Ayurveda luxuries is the result of nearly 50 years of expertise in the field of natural health & beauty products from the pioneer in modern Ayurveda beauty care in Sri Lanka.",
+    link: "https://spaceylonnewzealand.co.nz/",
+  },
+  {
+    name: "Shaalis",
+    img: "https://cknickbo.sirv.com/Images/tinified/Shaalis.png",
+    desctiption: "UK retailer with customized designs for them",
+    link: "https://www.shaalis.com/",
+  },
+  {
+    name: "PetZon ",
+    img: "https://cknickbo.sirv.com/Images/tinified/PetZon.png",
+    desctiption: "Love puppies, a store made with love",
+    link: "https://www.youtube.com/watch?v=cbpKAURo-0A",
+  },
+  {
+    name: "Aliya.lk ",
+    img: "https://cknickbo.sirv.com/Images/tinified/aliya.png",
+    desctiption:
+      "Where fashion meets the digtal space, expanding sales with passion",
+    link: "https://aliya.lk/",
+  },
+  {
+    name: "Troublemaker",
+    img: "https://cknickbo.sirv.com/Images/tinified/TroubleMaker.png",
+    desctiption: "Online baby store for little troublemakers",
+    link: "https://troublemaker.lk/",
+  },
+  {
+    name: "Miss N lingerie",
+    img: "https://cknickbo.sirv.com/Images/tinified/MissN.png",
+    desctiption: "Find a large open for lingeries on cyberspace",
+    link: "https://missnlingerie.com/",
+  },
+  {
+    name: "Aveves",
+    img: "https://cknickbo.sirv.com/Images/tinified/Avevus.png",
+    desctiption:
+      "Aveves Beauty Store with elegance design that enhance the product promotions",
+    link: "https://www.behance.net/gallery/153826611/Aveves-Shopify-Dropshipping-Website",
+  },
+  {
+    name: "Cyber Grips",
+    img: "https://cknickbo.sirv.com/Images/tinified/cyber%20grips.png",
+    desctiption: "Win the competiton with Gaming web store",
+    link: "https://www.youtube.com/watch?v=bnLTsXUdb-g",
+  },
+  {
+    name: "GPU Store",
+    img: "https://cknickbo.sirv.com/Images/tinified/gpu.png",
+    desctiption: "GPU Store, a perfect e-commerce store for quality graphics",
+    link: "https://www.youtube.com/watch?v=wCgV2oZRpvE",
+  },
+  {
+    name: "Bikerzcape",
+    img: "https://cknickbo.sirv.com/Images/tinified/Bikerscape.png",
+    desctiption: "Ride the market journey",
+    link: "https://www.youtube.com/watch?v=dRDJTKleLkM",
+  },
+  {
+    name: "Zip It Pro",
+    img: "https://cknickbo.sirv.com/Images/tinified/ZipIt%20Pro.png",
+    desctiption: "Simplifying plastic needs with simple store",
+    link: "https://www.youtube.com/watch?v=fSJWwUEq55o",
+  },
+  {
+    name: "Tempura Frying Pot",
+    img: "https://cknickbo.sirv.com/Images/tinified/Tempura.png",
+    desctiption: "A right pot targeting market spot",
+    link: "https://www.youtube.com/watch?v=3npmx0u_qCo",
+  },
+];
+
+const contactConfig = {
+  YOUR_EMAIL: "hello@lakshitha.live",
+  YOUR_FONE: "(94)774457833",
+  description:
+    "Design your shopify and wordpress e-commerce sites with years of experiance serving hundreds of clients worldwide",
+  // creat an emailjs.com account
+  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  YOUR_SERVICE_ID: "service_9pfi4vf",
+  YOUR_TEMPLATE_ID: "template_q0c0dqf",
+  YOUR_USER_ID: "-0aLpQuaTJ8DhByze",
+};
+
+const socialprofils = {
+  facebook:
+    "https://www.facebook.com/people/Lakshitha-Weerakoon/100009058018671/",
+  linkedin: "https://www.linkedin.com/in/lakshitha-weerakoon/",
+  twitter: "https://www.instagram.com/vikke___/",
+  youtube: "https://www.youtube.com/channel/UCgm8nLe7EMgM13muil8eyhQ/videos",
+  twitch: "https://www.behance.net/lakshithavikum",
+};
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+};
